a01912c2002933c2c318362b964922105ba622c5
mrProgrammer <farzadmohamadi201@gmail.com>
2023-09-16 12:01:48 +0330

    Implement EFCore

---
+++ DAL/DataBase/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.DataBase
+{
+    public class DataBaseContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog = DBContact;Integrated Security= True");
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
--- DAL/Entities/Contact.cs
+++ DAL/Entities/Contact.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DAL
+namespace DAL.Entities
 {
-    internal class Contact
+    public class Contact
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateAt { get; set; }
     }
 }
